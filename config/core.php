fb451cfcbb4096b4ff617001578d49245a3397cd
matthieukt <kwant.matthieu@gmail.com>
2019-01-25 19:16:31 +0100

    Ajout des réglages de base, le fichier core.php et le login_checker

---
+++ config/core.php
@@ -0,0 +1,10 @@
+<?php
+// Montre le rapport d'erreurs
+error_reporting(E_ALL);
+// Démarre une session php
+session_start();
+// votre fuseau horaire
+date_default_timezone_set('Europe/Paris');
+// L'URL de la page d'acceuil
+$home_url="http://localhost/reprise/";
+?>
